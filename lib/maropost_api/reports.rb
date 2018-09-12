295e7d129b87613aca6a058883cc4464098bfbff
Egor Romanov <unsaidxpl@gmail.com>
2018-09-12 17:09:36 +0300

    make params non mandatory

--- lib/maropost_api/reports.rb
+++ lib/maropost_api/reports.rb
@@ -1,11 +1,11 @@
 module MaropostApi
   class Reports < Resource
-    def opens(params)
+    def opens(params = {})
       response = @request.get(endpoint: "/reports/opens.json?#{params.to_query}")
       Response.new(response: response, parser: Parser::CollectionParser.new).call
     end
 
-    def clicks(params)
+    def clicks(params = {})
       response = @request.get(endpoint: "/reports/clicks.json?#{params.to_query}")
       Response.new(response: response, parser: Parser::CollectionParser.new).call
     end
